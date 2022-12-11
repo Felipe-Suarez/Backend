331eec47d4a63b2b074aef4852af43e3a1afcdb4
Felipe Suarez <felipesuarez1525@gmail.com>
2022-12-11 10:23:58 -0300

    update css and add mediaquerys in chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,9 +10,6 @@ openBtn.addEventListener('click', () => {
     openBtn.style = 'transform: translateY(150%)'
     setTimeout(() => {
         chatContainer.style = 'transform: translateY(0)'
-        header.style = 'filter: blur(1px)'
-        main.style = 'filter: blur(1px)'
-        document.body.style = 'overflow: hidden'
     }, 200)
 })
 
@@ -20,8 +17,5 @@ closeBtn.addEventListener('click', () => {
     chatContainer.style = 'transform: translateY(100%)'
     setTimeout(() => {
         openBtn.style = 'transform: translateY(0)'
-        header.style = 'filter: blur(0)'
-        main.style = 'filter: blur(0)'
-        document.body.style = 'overflow: unset'
     }, 200)
 })
