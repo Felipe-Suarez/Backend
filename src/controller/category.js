7eadfe9ae49b3c7bdc9819fd5e13e44d46094b50
Felipe Suarez <felipesuarez1525@gmail.com>
2022-12-22 02:55:51 -0300

    add favicon, categorys msg and change input tag to select for categories

--- src/controller/category.js
+++ src/controller/category.js
@@ -10,11 +10,14 @@ route.get('/', async (req, res) => {
     const name = userData?.username.toUpperCase()
     const veifyAdmin = await isAdmin(req)
 
+    const msg = 'mira nuestros productos'
+
     res.render('category', {
         userInfo: userData,
         username: name,
         productsInfo,
-        userAdmin: veifyAdmin
+        userAdmin: veifyAdmin,
+        msg
     })
 })
 
@@ -26,11 +29,19 @@ route.get('/:categoryName', async (req, res) => {
     const category = req.params.categoryName
     const products = await serviceCategory(category)
 
+    let msg;
+    if (category === 'mates' || category === 'termos') {
+        msg = `mira los ${category} que tenemos para vos`
+    } else {
+        msg = `mira las ${category} que tenemos para vos`
+    }
+
     res.render('category', {
         userInfo: userData,
         username: name,
         productsInfo: products,
-        userAdmin: veifyAdmin
+        userAdmin: veifyAdmin,
+        msg
     })
 })
 
