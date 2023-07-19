3ec5ce77217742886123ab102fd001c35da3d952
Felipe Suarez <felipesuarez1525@gmail.com>
2023-07-19 00:55:36 -0300

    comment cluster

--- server.js
+++ server.js
@@ -26,101 +26,101 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import cluster from 'cluster'
-import CPUs from 'os'
-const numCPUs = CPUs.cpus().length
+// import cluster from 'cluster'
+// import CPUs from 'os'
+// const numCPUs = CPUs.cpus().length
 
-if (cluster.isPrimary) {
-    logger.info("num CPUs: " + numCPUs);
-    logger.info(`I am a master ${process.pid}`);
+// if (cluster.isPrimary) {
+//     logger.info("num CPUs: " + numCPUs);
+//     logger.info(`I am a master ${process.pid}`);
 
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-    cluster.on("exit", (worker) => {
-        logger.info(`${worker.process.pid} is finished`);
-    });
+//     for (let i = 0; i < numCPUs; i++) {
+//         cluster.fork();
+//     }
+//     cluster.on("exit", (worker) => {
+//         logger.info(`${worker.process.pid} is finished`);
+//     });
 
-} else {
+// } else {
 
-    //init
-    const app = express();
+//init
+const app = express();
 
-    //compression gzip
-    app.use(compression({ filter: shouldCompress }))
-    function shouldCompress(req, res) {
+//compression gzip
+app.use(compression({ filter: shouldCompress }))
+function shouldCompress(req, res) {
 
-        // don't compress responses with this request header
-        if (req.headers['x-no-compression']) { return false }
+    // don't compress responses with this request header
+    if (req.headers['x-no-compression']) { return false }
 
-        // fallback to standard filter function
-        return compression.filter(req, res)
-    }
-
-    //cookies & session
-    app.use(cookieParser());
-
-    const options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    };
-
-    app.use(
-        session({
-            store: new MongoStore({
-                mongoUrl: URL_MONGO,
-                mongoOptions: options
-            }),
-            secret: SECRET,
-            resave: true,
-            saveUninitialized: true,
-            rolling: true,
-            cookie: { maxAge: Number(MAX_AGE) }
-        })
-    );
-
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    //ejs
-    app.set('views', './src/views');
-    app.set('view engine', 'ejs')
-
-    //middlewares
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use('/', router)
-
-    // SOCKET
-    const httpServer = new HttpServer(app)
-    const io = new IOServer(httpServer)
-
-    io.on('connection', async function (socket) {
-        logger.info('Usuario conectado');
-
-        const messages = await chatDao.list();
-        socket.emit('messages', messages)
-
-        socket.on('new-message', async function (data) {
-            try {
-                chatDao.save(data)
-                const messages = await chatDao.list()
-                io.sockets.emit('messages', messages)
-            } catch (err) {
-                logger.error(err)
-            }
-        })
-        socket.on('disconnect', () => {
-            logger.info('Usuario desconectado');
-        });
-    })
-
-    let usePort = process.env.PORT || 8080
-    // if (NODE_ENV === 'prod') usePort = PORT
-    // else usePort = 8080
-
-    //listen
-    httpServer.listen(usePort, () => logger.info(`Escuchando al puerto ${usePort}. Utilizando ${useDB}`))
-    httpServer.on('error', (err) => logger.error(err))
+    // fallback to standard filter function
+    return compression.filter(req, res)
 }
+
+//cookies & session
+app.use(cookieParser());
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+app.use(
+    session({
+        store: new MongoStore({
+            mongoUrl: URL_MONGO,
+            mongoOptions: options
+        }),
+        secret: SECRET,
+        resave: true,
+        saveUninitialized: true,
+        rolling: true,
+        cookie: { maxAge: Number(MAX_AGE) }
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//ejs
+app.set('views', './src/views');
+app.set('view engine', 'ejs')
+
+//middlewares
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', router)
+
+// SOCKET
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+io.on('connection', async function (socket) {
+    logger.info('Usuario conectado');
+
+    const messages = await chatDao.list();
+    socket.emit('messages', messages)
+
+    socket.on('new-message', async function (data) {
+        try {
+            chatDao.save(data)
+            const messages = await chatDao.list()
+            io.sockets.emit('messages', messages)
+        } catch (err) {
+            logger.error(err)
+        }
+    })
+    socket.on('disconnect', () => {
+        logger.info('Usuario desconectado');
+    });
+})
+
+let usePort = process.env.PORT || 8080
+// if (NODE_ENV === 'prod') usePort = PORT
+// else usePort = 8080
+
+//listen
+httpServer.listen(usePort, () => logger.info(`Escuchando al puerto ${usePort}. Utilizando ${useDB}`))
+httpServer.on('error', (err) => logger.error(err))
+// }
